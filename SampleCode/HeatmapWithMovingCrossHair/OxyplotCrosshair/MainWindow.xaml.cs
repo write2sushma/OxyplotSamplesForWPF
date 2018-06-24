f5c7ad1a38e486a9df1a93cb82e0f1ee09c2f485
Sushma <sushma.goutam@gmail.com>
2018-06-24 12:49:38 +0200

    Oxyplot heatmap with moving crosshair

---
+++ SampleCode/HeatmapWithMovingCrossHair/OxyplotCrosshair/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using OxyPlot;
+using OxyPlot.Annotations;
+using OxyPlot.Axes;
+using System.Windows;
+using System.Windows.Media;
+
+namespace OxyplotCrosshair
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// View Model Instance
+        /// </summary>
+        private readonly MainWindowViewModel _viewModel;
+
+        private OxyPlot.Wpf.LineAnnotation crosshair_v;
+        private OxyPlot.Wpf.LineAnnotation crosshair_h;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _viewModel = new MainWindowViewModel();
+            this.DataContext = _viewModel;
+
+            InitializeCrossHair();
+            BindMouseClick();
+        }
+
+        private void InitializeCrossHair()
+        {
+            // Add Cross hair to each plot
+            crosshair_v = new OxyPlot.Wpf.LineAnnotation
+            {
+                Type = LineAnnotationType.Vertical,
+                Color = Color.FromRgb(135, 206, 250),
+                StrokeThickness = 1.05,
+                LineStyle = LineStyle.Solid,
+                X = 0.5,
+                Y = 0
+            };
+            crosshair_h = new OxyPlot.Wpf.LineAnnotation
+            {
+                Type = LineAnnotationType.Horizontal,
+                Color = Color.FromRgb(135, 206, 250),
+                StrokeThickness = 1.05,
+                LineStyle = LineStyle.Solid,
+                X = 0,
+                Y = 0.5
+            };
+            CrossHairPlot.Annotations.Add(crosshair_v);
+            CrossHairPlot.Annotations.Add(crosshair_h);
+        }
+
+        private void BindMouseClick()
+        {
+            var controller = CrossHairPlot.ActualController;
+            var handleClick = new DelegatePlotCommand<OxyMouseDownEventArgs>(
+                (v, c, e) =>
+                {
+                    var args = new HitTestArguments(e.Position, 0);
+
+                    Axis yAxis = CrossHairPlot.ActualModel.Axes[1];
+                    DataPoint dataPoint = CrossHairPlot.ActualModel.DefaultXAxis.InverseTransform(args.Point.X, args.Point.Y, yAxis);
+
+                    // Update cross hair position according to new click
+                    crosshair_v.X = dataPoint.X;
+                    crosshair_v.Y = 0;
+                    crosshair_h.X = 0;
+                    crosshair_h.Y = dataPoint.Y;
+                    CrossHairPlot.InvalidatePlot(false);
+
+                    e.Handled = true;
+                });
+            controller.Bind(new OxyMouseDownGesture(OxyMouseButton.Left), handleClick);
+        }        
+    }
+}
---
+++ SampleCode/HeatmapWithMovingCrossHair/OxyplotCrosshair/MainWindowViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace OxyplotCrosshair
+{
+    internal class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+            MinX = 0;
+            MinY = 0;
+            MaxX = 1;
+            MaxY = 1;
+
+            MyData = GenerateHeatMap();
+        }
+        public double[,] MyData { get; set; }
+
+        public int MinX { get; set; }
+        public int MinY { get; set; }
+        public int MaxX { get; set; }
+        public int MaxY { get; set; }
+
+        /// <summary>
+        /// Generates the heat map data.
+        /// </summary>
+        /// <returns>
+        /// The heat map data array.
+        /// </returns>
+        private double[,] GenerateHeatMap()
+        {
+            const int rows = 11;
+            const int cols = 11;
+            var result = new double[rows, cols];
+            for (var i = 0; i < rows; i++)
+            {
+                for (var j = 0; j < cols; j++)
+                {
+                    result[i, j] = Math.Sin(4 * Math.PI * i / rows) * Math.Sqrt(200 * Math.PI * j / cols);
+                }
+            }
+
+            return result;
+        }
+    }
+}
